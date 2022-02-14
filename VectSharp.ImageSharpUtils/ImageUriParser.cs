dc5730f5abe8bbe51ffccdee7aa30442da935558
Giorgio Bianchini <giorgio.bianchini@bristol.ac.uk>
2022-02-14 02:43:39 +0100

    Added VectSharp.ImageSharpUtils.

---
+++ VectSharp.ImageSharpUtils/ImageUriParser.cs
@@ -0,0 +1,89 @@
+﻿/*
+    VectSharp - A light library for C# vector graphics.
+    Copyright (C) 2020-2022 Giorgio Bianchini
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using SixLabors.ImageSharp;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace VectSharp.ImageSharpUtils
+{
+    /// <summary>
+    /// Provides a method to parse an image URI into a page.
+    /// </summary>
+    public static class ImageURIParser
+    {
+        /// <summary>
+        /// Parses an image URI into a page. This is intended to replace the default image URI interpreter in <c>VectSharp.SVG.Parser.ParseImageURI</c>. To do this, use something like:
+        /// <code>VectSharp.SVG.Parser.ParseImageURI = VectSharp.ImageSharpUtils.ImageURIParser.Parser(VectSharp.SVG.Parser.ParseSVGURI);</code>
+        /// </summary>
+        /// <param name="parseSVG">A function to parse an SVG image uri into a page. You should pass <c>VectSharp.SVG.Parser.ParseSVGURI</c> as this argument.</param>
+        /// <returns>A function to parse an image URI into a page.</returns>
+        public static Func<string, bool, Page> Parser(Func<string, bool, Page> parseSVG)
+        {
+            return (string uri, bool interpolate) =>
+            {
+                if (uri.StartsWith("data:"))
+                {
+                    string mimeType = uri.Substring(uri.IndexOf(":") + 1, uri.IndexOf(";") - uri.IndexOf(":") - 1);
+
+                    string type = uri.Substring(uri.IndexOf(";") + 1, uri.IndexOf(",") - uri.IndexOf(";") - 1);
+
+                    if (mimeType != "image/svg+xml")
+                    {
+                        int offset = uri.IndexOf(",") + 1;
+
+                        byte[] parsed;
+
+                        string substring = uri.Substring(offset);
+
+                        switch (type)
+                        {
+                            case "base64":
+                                parsed = Convert.FromBase64String(uri.Substring(offset));
+                                break;
+                            case "":
+                                parsed = (from el in System.Web.HttpUtility.UrlDecode(uri.Substring(offset)) select (byte)el).ToArray();
+                                break;
+                            default:
+                                throw new InvalidDataException("Unknown data stream type!");
+                        }
+
+                        GCHandle handle = GCHandle.Alloc(parsed, GCHandleType.Pinned);
+
+                        RasterImageStream img = new RasterImageStream(handle.AddrOfPinnedObject(), parsed.Length, interpolate: interpolate);
+
+                        handle.Free();
+
+                        Page pag = new Page(img.Width, img.Height);
+
+                        pag.Graphics.DrawRasterImage(0, 0, img);
+
+                        return pag;
+                    }
+                    else
+                    {
+                        return parseSVG(uri, interpolate);
+                    }
+                }
+
+                return null;
+            };
+        }
+    }
+}
---
+++ VectSharp.ImageSharpUtils/RasterImages.cs
@@ -0,0 +1,251 @@
+﻿/*
+    VectSharp - A light library for C# vector graphics.
+    Copyright (C) 2020-2022 Giorgio Bianchini
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace VectSharp.ImageSharpUtils
+{
+    /// <summary>
+    /// A <see cref="RasterImage"/> created from a file.
+    /// </summary>
+    public class RasterImageFile : RasterImage
+    {
+        /// <summary>
+        /// Creates a new <see cref="RasterImage"/> from the specified file.
+        /// </summary>
+        /// <param name="fileName">The path to the file containing the image.</param>
+        /// <param name="alpha">A boolean value indicating whether transparency (alpha) data from the image should be preserved or not.</param>
+        /// <param name="interpolate">A boolean value indicating whether the image should be interpolated when it is resized or not.</param>
+        public RasterImageFile(string fileName, bool alpha = true, bool interpolate = true)
+        {
+            Image image;
+
+            if (alpha)
+            {
+                image = Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(fileName, out SixLabors.ImageSharp.Formats.IImageFormat format);
+            }
+            else
+            {
+                image = Image.Load<SixLabors.ImageSharp.PixelFormats.Rgb24>(fileName, out SixLabors.ImageSharp.Formats.IImageFormat format);
+            }
+
+            image.Mutate(x => x.AutoOrient());
+
+            int stride = image.Width * (alpha ? 4 : 3);
+            int size = stride * image.Height;
+
+            IntPtr tbr = Marshal.AllocHGlobal(size);
+            GC.AddMemoryPressure(size);
+
+            IntPtr pointer = tbr;
+
+            unsafe
+            {
+                if (alpha)
+                {
+                    Image<SixLabors.ImageSharp.PixelFormats.Rgba32> img = (Image<SixLabors.ImageSharp.PixelFormats.Rgba32>)image;
+
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Memory<SixLabors.ImageSharp.PixelFormats.Rgba32> row = img.DangerousGetPixelRowMemory(y);
+
+                        Span<SixLabors.ImageSharp.PixelFormats.Rgba32> newRow = new Span<SixLabors.ImageSharp.PixelFormats.Rgba32>(pointer.ToPointer(), row.Length);
+                        row.Span.CopyTo(newRow);
+
+                        pointer = IntPtr.Add(pointer, stride);
+                    }
+                }
+                else
+                {
+                    Image<SixLabors.ImageSharp.PixelFormats.Rgb24> img = (Image<SixLabors.ImageSharp.PixelFormats.Rgb24>)image;
+
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Memory<SixLabors.ImageSharp.PixelFormats.Rgb24> row = img.DangerousGetPixelRowMemory(y);
+
+                        Span<SixLabors.ImageSharp.PixelFormats.Rgb24> newRow = new Span<SixLabors.ImageSharp.PixelFormats.Rgb24>(pointer.ToPointer(), row.Length);
+                        row.Span.CopyTo(newRow);
+
+                        pointer = IntPtr.Add(pointer, stride);
+                    }
+                }
+            }
+
+            this.Width = image.Width;
+            this.Height = image.Height;
+            this.HasAlpha = alpha;
+            this.Interpolate = interpolate;
+            this.Id = Guid.NewGuid().ToString();
+            this.ImageDataAddress = tbr;
+            this.DataHolder = new DisposableIntPtr(tbr);
+        }
+    }
+
+    /// <summary>
+    /// A <see cref="RasterImage"/> created from a stream.
+    /// </summary>
+    public class RasterImageStream : RasterImage
+    {
+        /// <summary>
+        /// Creates a new <see cref="RasterImage"/> from the specified stream.
+        /// </summary>
+        /// <param name="imageStream">The stream containing the image data.</param>
+        /// <param name="alpha">A boolean value indicating whether transparency (alpha) data from the image should be preserved or not.</param>
+        /// <param name="interpolate">A boolean value indicating whether the image should be interpolated when it is resized or not.</param>
+        public RasterImageStream(Stream imageStream, bool alpha = true, bool interpolate = true)
+        {
+            Image image;
+
+            if (alpha)
+            {
+                image = Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(imageStream, out SixLabors.ImageSharp.Formats.IImageFormat format);
+            }
+            else
+            {
+                image = Image.Load<SixLabors.ImageSharp.PixelFormats.Rgb24>(imageStream, out SixLabors.ImageSharp.Formats.IImageFormat format);
+            }
+
+            image.Mutate(x => x.AutoOrient());
+
+            int stride = image.Width * (alpha ? 4 : 3);
+            int size = stride * image.Height;
+
+            IntPtr tbr = Marshal.AllocHGlobal(size);
+            GC.AddMemoryPressure(size);
+
+            IntPtr pointer = tbr;
+
+            unsafe
+            {
+                if (alpha)
+                {
+                    Image<SixLabors.ImageSharp.PixelFormats.Rgba32> img = (Image<SixLabors.ImageSharp.PixelFormats.Rgba32>)image;
+
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Memory<SixLabors.ImageSharp.PixelFormats.Rgba32> row = img.DangerousGetPixelRowMemory(y);
+
+                        Span<SixLabors.ImageSharp.PixelFormats.Rgba32> newRow = new Span<SixLabors.ImageSharp.PixelFormats.Rgba32>(pointer.ToPointer(), row.Length);
+                        row.Span.CopyTo(newRow);
+
+                        pointer = IntPtr.Add(pointer, stride);
+                    }
+                }
+                else
+                {
+                    Image<SixLabors.ImageSharp.PixelFormats.Rgb24> img = (Image<SixLabors.ImageSharp.PixelFormats.Rgb24>)image;
+
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Memory<SixLabors.ImageSharp.PixelFormats.Rgb24> row = img.DangerousGetPixelRowMemory(y);
+
+                        Span<SixLabors.ImageSharp.PixelFormats.Rgb24> newRow = new Span<SixLabors.ImageSharp.PixelFormats.Rgb24>(pointer.ToPointer(), row.Length);
+                        row.Span.CopyTo(newRow);
+
+                        pointer = IntPtr.Add(pointer, stride);
+                    }
+                }
+            }
+
+            this.Width = image.Width;
+            this.Height = image.Height;
+            this.HasAlpha = alpha;
+            this.Interpolate = interpolate;
+            this.Id = Guid.NewGuid().ToString();
+            this.ImageDataAddress = tbr;
+            this.DataHolder = new DisposableIntPtr(tbr);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="RasterImage"/> from the specified stream.
+        /// </summary>
+        /// <param name="imageAddress">A pointer to the address where the image data is contained.</param>
+        /// <param name="imageLength">The length in bytes of the image data.</param>
+        /// <param name="alpha">A boolean value indicating whether transparency (alpha) data from the image should be preserved or not.</param>
+        /// <param name="interpolate">A boolean value indicating whether the image should be interpolated when it is resized or not.</param>
+        public RasterImageStream(IntPtr imageAddress, int imageLength, bool alpha = true, bool interpolate = true)
+        {
+            unsafe
+            {
+                ReadOnlySpan<byte> imageSpan = new ReadOnlySpan<byte>((void*)imageAddress, imageLength);
+
+                Image image;
+
+                if (alpha)
+                {
+                    image = Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(imageSpan, out SixLabors.ImageSharp.Formats.IImageFormat format);
+                }
+                else
+                {
+                    image = Image.Load<SixLabors.ImageSharp.PixelFormats.Rgb24>(imageSpan, out SixLabors.ImageSharp.Formats.IImageFormat format);
+                }
+
+                image.Mutate(x => x.AutoOrient());
+
+                int stride = image.Width * (alpha ? 4 : 3);
+                int size = stride * image.Height;
+
+                IntPtr tbr = Marshal.AllocHGlobal(size);
+                GC.AddMemoryPressure(size);
+
+                IntPtr pointer = tbr;
+
+                if (alpha)
+                {
+                    Image<SixLabors.ImageSharp.PixelFormats.Rgba32> img = (Image<SixLabors.ImageSharp.PixelFormats.Rgba32>)image;
+
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Memory<SixLabors.ImageSharp.PixelFormats.Rgba32> row = img.DangerousGetPixelRowMemory(y);
+
+                        Span<SixLabors.ImageSharp.PixelFormats.Rgba32> newRow = new Span<SixLabors.ImageSharp.PixelFormats.Rgba32>(pointer.ToPointer(), row.Length);
+                        row.Span.CopyTo(newRow);
+
+                        pointer = IntPtr.Add(pointer, stride);
+                    }
+                }
+                else
+                {
+                    Image<SixLabors.ImageSharp.PixelFormats.Rgb24> img = (Image<SixLabors.ImageSharp.PixelFormats.Rgb24>)image;
+
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Memory<SixLabors.ImageSharp.PixelFormats.Rgb24> row = img.DangerousGetPixelRowMemory(y);
+
+                        Span<SixLabors.ImageSharp.PixelFormats.Rgb24> newRow = new Span<SixLabors.ImageSharp.PixelFormats.Rgb24>(pointer.ToPointer(), row.Length);
+                        row.Span.CopyTo(newRow);
+
+                        pointer = IntPtr.Add(pointer, stride);
+                    }
+                }
+
+                this.Width = image.Width;
+                this.Height = image.Height;
+                this.HasAlpha = alpha;
+                this.Interpolate = interpolate;
+                this.Id = Guid.NewGuid().ToString();
+                this.ImageDataAddress = tbr;
+                this.DataHolder = new DisposableIntPtr(tbr);
+            }
+        }
+    }
+}
